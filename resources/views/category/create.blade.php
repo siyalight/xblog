4e46d3013cb3a23647cc8caa6d08e49ba8d26669
lufficc <luffy.lcc@gmail.com>
2016-08-17 11:55:26 +0800

    commit

--- resources/views/category/create.blade.php
+++ resources/views/category/create.blade.php
@@ -1,29 +1,28 @@
-@extends('layouts.app)
+@extends('layouts.app')
 @section('content')
-
-    <div class="container">
-        <div class="box box-primary box-solid">
-            <div class="box-header">创建一个新的分类</div>
-            <div class="box-body">
-                <form class="form-vertical" action="/admin/category" method="post">
-                    <div class="input-group">
-                        <span class="input-group-addon">名字*</span>
-                        <input type="text" name="name" class="form-control" id="name" value="${categoryForm.name ! ""}">
-                    </div>
-                    <br>
-                    <div class="input-group">
-                        <span class="input-group-addon">描述*</span>
-                        <input type="text" name="description" class="form-control" id="description"
-                               value="${categoryForm.description ! ""}"/>
-                    </div>
-                    <br>
-                    <div class="input-group">
-                        <button type="submit" class="btn btn-success">创建</button>
-                    </div>
-                </form>
+    <div class="widget widget-default">
+        <div class="container">
+            <div class="box box-primary box-solid">
+                <div class="box-header">创建一个新的分类</div>
+                <div class="box-body">
+                    <form class="form-vertical" action="{{ route('category.store') }}" method="post">
+                        <div class="input-group">
+                            <span class="input-group-addon">名字*</span>
+                            <input type="text" name="name" class="form-control" id="name">
+                        </div>
+                        <br>
+                        <div class="input-group">
+                            <span class="input-group-addon">描述*</span>
+                            <input type="text" name="description" class="form-control" id="description"/>
+                        </div>
+                        <br>
+                        {{ csrf_field() }}
+                        <div class="input-group">
+                            <button type="submit" class="btn btn-success">创建</button>
+                        </div>
+                    </form>
+                </div>
             </div>
-
         </div>
     </div>
-
 @endsection
