34dac57af72daf172ad84b6a1ca436aabc346dec
lufficc <luffy.lcc@gmail.com>
2016-08-17 22:24:32 +0800

    commit

---
+++ app/Http/Middleware/AdminMiddleware.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+
+class AdminMiddleware
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request $request
+     * @param  \Closure $next
+     * @return mixed
+     */
+    public function handle($request, Closure $next)
+    {
+        if (auth()->check() && auth()->user()->id == 1)
+            return $next($request);
+        abort(404);
+    }
+}
