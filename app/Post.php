ac5d3f7c6c0f57e4bd6898e057a50de3e8e8b839
lufficc <luffy.lcc@gmail.com>
2016-08-17 10:47:37 +0800

    add model

--- app/Post.php
+++ app/Post.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+
+    public function category()
+    {
+        return $this->belongsTo('App\Post');
+    }
+
+    public function tags()
+    {
+        return $this->hasMany('App\Tag');
+    }
 }
