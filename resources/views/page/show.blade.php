4be2463309a36a363eebb78d2f02ca04ab39f888
lufficc <luffy.lcc@gmail.com>
2016-08-20 13:22:17 +0800

    add status to posts table

--- resources/views/page/show.blade.php
+++ resources/views/page/show.blade.php
@@ -56,7 +56,7 @@
             var ds = document.createElement('script');
             ds.type = 'text/javascript';
             ds.async = true;
-            ds.src = ('https:') + '//static.duoshuo.com/embed.js';
+            ds.src = (document.location.protocol == 'https:' ? 'https:' : 'http:') + '//static.duoshuo.com/embed.js';
             ds.charset = 'UTF-8';
             (document.getElementsByTagName('head')[0]
             || document.getElementsByTagName('body')[0]).appendChild(ds);
