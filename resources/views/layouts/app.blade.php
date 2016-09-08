c3c1d50c5cc2d48efaaa1420107f376c526e2c24
lufficc <luffy.lcc@gmail.com>
2016-09-08 22:29:38 +0800

    commit

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -29,7 +29,7 @@
                 'csrfToken' => csrf_token(),
         ]); ?>
     </script>
-    {{--<script>
+    <script>
         (function (i, s, o, g, r, a, m) {
             i['GoogleAnalyticsObject'] = r;
             i[r] = i[r] || function () {
@@ -43,7 +43,7 @@
         })(window, document, 'script', 'https://www.google-analytics.com/analytics.js', 'ga');
         ga('create', 'UA-81713716-2', 'auto');
         ga('send', 'pageview');
-    </script>--}}
+    </script>
 
 </head>
 <body>
