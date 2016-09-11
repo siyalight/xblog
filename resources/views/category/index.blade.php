0199792c18f42cd50ba33c3a2e2f6a4381861d77
lufficc <luffy.lcc@gmail.com>
2016-09-11 21:11:18 +0800

    add tag

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -1,8 +1,12 @@
 @extends('layouts.app')
 @section('title','分类')
 @section('content')
+    <ol class="breadcrumb">
+        <li><a href="/">Home</a></li>
+        <li class="active">Category</li>
+    </ol>
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-12">
             @include('widget.categories')
         </div>
     </div>
--- resources/views/tag/index.blade.php
+++ resources/views/tag/index.blade.php
@@ -1,8 +1,12 @@
 @extends('layouts.app')
 @section('title','标签')
 @section('content')
+    <ol class="breadcrumb">
+        <li><a href="/">Home</a></li>
+        <li class="active">Tag</li>
+    </ol>
     <div class="row">
-        <div class="col-md-8 col-md-offset-2">
+        <div class="col-md-12">
             @include('widget.tags')
         </div>
     </div>
