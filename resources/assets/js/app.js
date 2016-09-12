71c0d3b43b819526af1c0f18c551ed4d9fb0bdb9
lufficc <luffy.lcc@gmail.com>
2016-09-13 02:03:51 +0800

    use pjax

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -2,35 +2,45 @@
  * @author lufficc
  */
 require('./footer')
-$(document).ready(function()
-{
-    $(document).pjax('a:not(a[target="_blank"])', '#lufficc-blog-container',{
-        timeout: 2000,
-        maxCacheLength:500,
-    });
-    $(document).on('pjax:complete', function() {
-        initMarkdownTarget();
-        initFooterPosition();
-        $(window).resize(initFooterPosition);
-    });
-});
-
-function initMarkdownTarget() {
-    var markdownTarget = document.getElementById('markdown-target');
-    if (markdownTarget)
-    {
-        markdownTarget.innerHTML =
-            marked($('#markdown-content').data("markdown"), {
-                renderer: new marked.Renderer(),
-                gfm: true,
-                tables: true,
-                breaks: false,
-                pedantic: false,
-                smartLists: true,
-                smartypants: false,
-                highlight: function (code) {
-                    return hljs.highlightAuto(code).value;
-                }
+(function ($) {
+    var LufficcBlog = {
+        init: function () {
+            var self = this;
+            $(document).pjax('a:not(a[target="_blank"])', '#lufficc-blog-container', {
+                timeout: 2000,
+                maxCacheLength: 500,
             });
+
+            $(document).on('pjax:complete', function () {
+                self.bootUp();
+            });
+        },
+        bootUp: function () {
+            self.initMarkdownTarget();
+            initFooterPosition();
+            $(window).resize(initFooterPosition);
+        },
+        initMarkdownTarget: function () {
+            var markdownTarget = document.getElementById('markdown-target');
+            if (markdownTarget) {
+                markdownTarget.innerHTML =
+                    marked($('#markdown-content').data("markdown"), {
+                        renderer: new marked.Renderer(),
+                        gfm: true,
+                        tables: true,
+                        breaks: false,
+                        pedantic: false,
+                        smartLists: true,
+                        smartypants: false,
+                        highlight: function (code) {
+                            return hljs.highlightAuto(code).value;
+                        }
+                    });
+            }
+        }
     }
-}
+    window.LufficcBlog = LufficcBlog;
+});
+$(document).ready(function () {
+    LufficcBlog.bootUp();
+});
