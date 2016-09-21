51869d622f7163268246f7332d77d82c4b26326d
lufficc <luffy.lcc@gmail.com>
2016-09-21 12:02:08 +0800

    add github

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -24,6 +24,16 @@ class RegisterController extends Controller
 
     public function register(Request $request)
     {
+        $this->validate($request, [
+            'name' => 'required|alpha_dash|max:16|min:3|unique:users',
+            'email' => 'required|email|max:255|unique:users',
+            'password' => 'required|min:6|confirmed',
+        ]);
+
+        if (mb_substr_count($request->get('name'), '_') > 1 || mb_substr_count($request->get('name'), '-') > 1) {
+            return back()->withErrors("name's '-' and '_' max count is 1.");
+        }
+
         $this->validator($request->all())->validate();
 
         auth()->login($this->create($request->all()));
