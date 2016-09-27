2d223d240afb9bf860bd5ecd7d8344544e068fd9
lufficc <luffy.lcc@gmail.com>
2016-09-27 19:11:44 +0800

    add post navigation ＃１

--- resources/assets/js/app.js
+++ resources/assets/js/app.js
@@ -33,12 +33,56 @@
             initDeleteTarget();
             highLightCode();
             initUploadAvatar();
-            initClickOnce();
+            initPostNavigation();
         },
     };
 
-    function initClickOnce() {
+    function initPostNavigation() {
+        var hs = $('.post-detail').find('h1,h2,h3,h4,h5,h6');
+        if (hs.length < 2)
+            return;
 
+        console.log('initPostNavigation')
+        var s = '';
+        s += '<div style="clear:both"></div>';
+       /* s += '<div class="cnblogs_toc">';*/
+        s += '<p style="text-align:right;margin:0;"><span style="float:left;">文章目录</span></p><br>';
+        s += '<ol style="margin-left:14px;padding-left:14px;line-height:160%;">';
+        var old_h = 0, ol_cnt = 0;
+        for (var i = 0; i < hs.length; i++) {
+            var h = parseInt(hs[i].tagName.substr(1), 10);
+            if (!old_h)
+                old_h = h;
+            if (h > old_h) {
+                s += '<ol>';
+                ol_cnt++;
+            }
+            else if (h < old_h && ol_cnt > 0) {
+                s += '</ol>';
+                ol_cnt--;
+            }
+            if (h == 1) {
+                while (ol_cnt > 0) {
+                    s += '</ol>';
+                    ol_cnt--;
+                }
+            }
+            old_h = h;
+            var tit = hs.eq(i).text().replace(/^\d+[.、\s]+/g, '');
+            tit = tit.replace(/[^a-zA-Z0-9_\-\s\u4e00-\u9fa5]+/g, '');
+
+            if (tit.length < 100) {
+                s += '<li><a href="#title' + i + '">' + tit + '</a></li>';
+                hs.eq(i).html('<a name="title' + i + '"></a>' + hs.eq(i).html());
+            }
+        }
+        while (ol_cnt > 0) {
+            s += '</ol>';
+            ol_cnt--;
+        }
+        s += '</ol>';
+        s += '<div style="clear:both"><br></div>';
+        $('#post-navigation').html(s);
     }
 
     function initDeleteTarget() {
