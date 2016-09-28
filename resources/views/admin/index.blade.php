99f9eeca358a378fe7711651e29ad37316f099cd
lufficc <luffy.lcc@gmail.com>
2016-09-29 04:26:27 +0800

    admin css changed

--- resources/views/admin/index.blade.php
+++ resources/views/admin/index.blade.php
@@ -2,97 +2,133 @@
 @section('title','Admin')
 @section('content')
     <div class="row">
-
-        <div class="col-lg-3 col-md-4">
-            <div class="widget widget-default center-block">
-                <div class="widget-header">
-                    <h3><i class="fa fa-user fa-fw"></i>User</h3>
+        <div class="col-md-3 col-sm-4 col-xs-6">
+            <a href="{{ route('admin.users') }}">
+                <div class="info-box">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            <div class="info-icon">
+                                <i class="fa fa-user fa-fw"></i>
+                            </div>
+                        </div>
+                        <div class="col-xs-8">
+                            <span>用户</span>
+                            <div class="info-title">{{ $info['user_count'] }}</div>
+                        </div>
+                    </div>
                 </div>
-                <div class="widget-body">
-                    <h3>
-                        <a href="{{ route('admin.users') }}">{{ $info['user_count'] }}</a>
-                    </h3>
-                </div>
-            </div>
+            </a>
         </div>
 
-        <div class="col-lg-3 col-md-4">
-            <div class="widget widget-default center-block">
-                <div class="widget-header">
-                    <h3><i class="fa fa-file fa-fw"></i>Page</h3>
+
+        <div class="col-md-3 col-sm-4 col-xs-6">
+            <a href="{{ route('admin.pages') }}">
+                <div class="info-box">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            <div class="info-icon">
+                                <i class="fa fa-file fa-fw"></i>
+                            </div>
+                        </div>
+                        <div class="col-xs-8">
+                            <span>页面</span>
+                            <div class="info-title">{{ $info['page_count'] }}</div>
+                        </div>
+                    </div>
                 </div>
-                <div class="widget-body">
-                    <h3>
-                        <a href="{{ route('admin.pages') }}">{{ $info['page_count'] }}</a>
-                    </h3>
-                </div>
-            </div>
+            </a>
         </div>
 
-        <div class="col-lg-3 col-md-4">
-            <div class="widget widget-default center-block">
-                <div class="widget-header">
-                    <h3><i class="fa fa-sticky-note fa-fw"></i>Post</h3>
+        <div class="col-md-3 col-sm-4 col-xs-6">
+            <a href="{{ route('admin.posts') }}">
+                <div class="info-box">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            <div class="info-icon">
+                                <i class="fa fa-sticky-note fa-fw"></i>
+                            </div>
+                        </div>
+                        <div class="col-xs-8">
+                            <span>文章</span>
+                            <div class="info-title">{{ $info['post_count'] }}</div>
+                        </div>
+                    </div>
                 </div>
-                <div class="widget-body">
-                    <h3>
-                        <a href="{{ route('admin.posts') }}">{{ $info['post_count'] }}</a>
-                    </h3>
-                </div>
-            </div>
+            </a>
         </div>
 
-        <div class="col-lg-3 col-md-4">
-            <div class="widget widget-default center-block">
-                <div class="widget-header">
-                    <h3><i class="fa fa-comments fa-fw"></i>Comment</h3>
+
+        <div class="col-md-3 col-sm-4 col-xs-6">
+            <a href="{{ route('admin.comments') }}">
+                <div class="info-box">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            <div class="info-icon">
+                                <i class="fa fa-comments fa-fw"></i>
+                            </div>
+                        </div>
+                        <div class="col-xs-8">
+                            <span>评论</span>
+                            <div class="info-title">{{ $info['comment_count'] }}</div>
+                        </div>
+                    </div>
                 </div>
-                <div class="widget-body">
-                    <h3>
-                        <a href="{{ route('admin.comments') }}">{{ $info['comment_count'] }}</a>
-                    </h3>
-                </div>
-            </div>
+            </a>
         </div>
 
-        <div class="col-lg-3 col-md-4">
-            <div class="widget widget-default center-block">
-                <div class="widget-header">
-                    <h3><i class="fa fa-tags fa-fw"></i>Tag</h3>
+
+        <div class="col-md-3 col-sm-4 col-xs-6">
+            <a href="{{ route('admin.tags') }}">
+                <div class="info-box">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            <div class="info-icon">
+                                <i class="fa fa-tags fa-fw"></i>
+                            </div>
+                        </div>
+                        <div class="col-xs-8">
+                            <span>标签</span>
+                            <div class="info-title">{{ $info['tag_count'] }}</div>
+                        </div>
+                    </div>
                 </div>
-                <div class="widget-body">
-                    <h3>
-                        <a href="{{route('admin.tags')}}">{{ $info['tag_count'] }}</a>
-                    </h3>
-                </div>
-            </div>
+            </a>
         </div>
 
-        <div class="col-lg-3 col-md-4">
-            <div class="widget widget-default center-block">
-                <div class="widget-header">
-                    <h3><i class="fa fa-folder fa-fw"></i>Category</h3>
+        <div class="col-md-3 col-sm-4 col-xs-6">
+            <a href="{{ route('admin.categories') }}">
+                <div class="info-box">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            <div class="info-icon">
+                                <i class="fa fa-folder fa-fw"></i>
+                            </div>
+                        </div>
+                        <div class="col-xs-8">
+                            <span>分类</span>
+                            <div class="info-title">{{ $info['category_count'] }}</div>
+                        </div>
+                    </div>
                 </div>
-                <div class="widget-body">
-                    <h3>
-                        <a href="{{ route('admin.categories') }}">{{ $info['category_count'] }}</a>
-                    </h3>
-                </div>
-            </div>
+            </a>
         </div>
 
-        <div class="col-lg-3 col-md-4">
-            <div class="widget widget-default center-block">
-                <div class="widget-header">
-                    <h3><i class="fa fa-image fa-fw"></i>Images</h3>
+        <div class="col-md-3 col-sm-4 col-xs-6">
+            <a href="{{ route('admin.images') }}">
+                <div class="info-box">
+                    <div class="row">
+                        <div class="col-xs-4">
+                            <div class="info-icon">
+                                <i class="fa fa-image fa-fw"></i>
+                            </div>
+                        </div>
+                        <div class="col-xs-8">
+                            <span>图片</span>
+                            <div class="info-title">{{ $info['image_count'] }}</div>
+                        </div>
+                    </div>
                 </div>
-                <div class="widget-body">
-                    <h3>
-                        <a href="{{ route('admin.images') }}">{{ $info['image_count'] }}</a>
-                    </h3>
-                </div>
-            </div>
+            </a>
         </div>
-
     </div>
 @endsection
--- resources/views/admin/settings.blade.php
+++ resources/views/admin/settings.blade.php
@@ -4,131 +4,127 @@
     <div class="row">
         <form role="form" id="setting-form" class="form-horizontal" action="{{ route('admin.save-settings') }}"
               method="post">
-            <div class="col-md-12">
-                <div class="widget widget-default">
-                    <div class="widget-header">
-                        <h3><i class="fa fa-cog fa-fw"></i>设置</h3>
+            <div class="widget widget-default">
+                <div class="widget-header">
+                    <h3><i class="fa fa-cog fa-fw"></i>设置</h3>
+                </div>
+                <div class="widget-body">
+                    <div class="form-group">
+                        <div class="radio">
+                            <label class="col-sm-offset-2">
+                                <input type="radio"
+                                       {{ isset($google_analytics) && $google_analytics == 'true' ? ' checked ':'' }}
+                                       name="google_analytics"
+                                       value="true">启用谷歌分析
+                            </label>
+                        </div>
+                        <div class="radio">
+                            <label class="col-sm-offset-2">
+                                <input type="radio"
+                                       {{ isset($google_analytics) && $google_analytics == 'true' ? '':' checked ' }}
+                                       name="google_analytics"
+                                       value="false">禁用谷歌分析
+                            </label>
+                        </div>
                     </div>
-                    <div class="widget-body">
-                        <div class="row">
-                            <div class="col-md-4">
-                                <div class="radio">
-                                    <label>
-                                        <input type="radio"
-                                               {{ isset($google_analytics) && $google_analytics == 'true' ? ' checked ':'' }}
-                                               name="google_analytics"
-                                               value="true">启用谷歌分析
-                                    </label>
-                                </div>
-                                <div class="radio">
-                                    <label>
-                                        <input type="radio"
-                                               {{ isset($google_analytics) && $google_analytics == 'true' ? '':' checked ' }}
-                                               name="google_analytics"
-                                               value="false">禁用谷歌分析
-                                    </label>
-                                </div>
-                                <div class="form-group">
-                                    <div class="input-group">
-                                        <label class="control-label">跟踪ID</label>
-                                        <input class="form-control" type="text" name="google_trace_id" value="{{ $google_trace_id or ''}}">
-                                    </div>
-                                </div>
-                            </div>
-                            <div class="col-md-8">
-                                <div class="form-group">
-                                    <div class="input-group">
-                                        <div class="input-group-addon">作者</div>
-                                        <input class="form-control" type="text" name="author" value="{{ $author or ''}}">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="input-group">
-                                        <div class="input-group-addon">描述</div>
-                                        <input class="form-control" type="text" name="description"
-                                               value="{{ $description or ''}}">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="input-group">
-                                        <div class="input-group-addon">头像</div>
-                                        <input class="form-control" type="text" name="avatar" value="{{ $avatar or ''}}">
-                                    </div>
-                                </div>
-                            </div>
+                    <div class="form-group">
+                        <label for="google_trace_id" class="col-sm-2 control-label">跟踪ID</label>
+                        <div class="col-sm-10">
+                            <input type="text" name="google_trace_id" class="form-control" id="google_trace_id" placeholder="谷歌跟踪ID"
+                                   value="{{ $google_trace_id or ''}}">
                         </div>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <div class="form-group">
-                                    <div class="input-group">
-                                        <div class="input-group-addon">Js</div>
-                                        <input class="form-control" type="text" name="site_js"
-                                               value="{{ $site_js or ''}}">
-                                    </div>
-                                </div>
+                    </div>
 
-                                <div class="form-group">
-                                    <div class="input-group">
-                                        <div class="input-group-addon">Css</div>
-                                        <input class="form-control" type="text" name="site_css"
-                                               value="{{ $site_css or ''}}">
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <div class="input-group">
-                                        <div class="input-group-addon">标题</div>
-                                        <input class="form-control" type="text" name="site_title"
-                                               value="{{ $site_title or ''}}">
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <div class="input-group">
-                                        <div class="input-group-addon">关键字</div>
-                                        <input placeholder="网站关键字" class="form-control" type="text" name="site_keywords"
-                                               value="{{ $site_keywords or ''}}">
-                                    </div>
-                                </div>
-
-                                <div class="form-group">
-                                    <div class="input-group">
-                                        <div class="input-group-addon">网站描述</div>
-                                        <input class="form-control" type="text" name="site_description"
-                                               value="{{ $site_description or '' }}">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="input-group">
-                                        <div class="input-group-addon">每页数量</div>
-                                        <input class="form-control" type="number" name="page_size"
-                                               value="{{ $page_size or 7 }}">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="input-group">
-                                        <div class="input-group-addon">简介图片</div>
-                                        <input class="form-control" type="text" name="profile_image"
-                                               value="{{ $profile_image or ''}}">
-                                    </div>
-                                </div>
-                                <div class="form-group">
-                                    <div class="input-group">
-                                        <div class="input-group-addon">背景图片</div>
-                                        <input class="form-control" type="text" name="background_image"
-                                               value="{{ $background_image or ''}}">
-                                    </div>
-                                </div>
-                            </div>
+                    <div class="form-group">
+                        <label for="author" class="col-sm-2 control-label">作者</label>
+                        <div class="col-sm-10">
+                            <input type="text" name="author" class="form-control" id="author"
+                                   value="{{ $author or ''}}">
                         </div>
-                        {{ csrf_field() }}
-                        <br>
-                        <div class="row">
-                            <div class="col-md-12">
-                                <button type="submit" class="btn bg-primary">
-                                    保存
-                                </button>
-                            </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="description" class="col-sm-2 control-label">描述</label>
+                        <div class="col-sm-10">
+                            <input type="text" name="description" class="form-control" id="description"
+                                   value="{{ $description or ''}}">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label for="avatar" class="col-sm-2 control-label">头像</label>
+                        <div class="col-sm-10">
+                            <input type="text" name="avatar" class="form-control" id="avatar"
+                                   value="{{ $avatar or ''}}">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">Js</label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="text" name="site_js"
+                                   value="{{ $site_js or ''}}">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">Css</label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="text" name="site_css"
+                                   value="{{ $site_css or ''}}">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">标题</label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="text" name="site_title"
+                                   value="{{ $site_title or ''}}">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">关键字</label>
+                        <div class="col-sm-10">
+                            <input placeholder="网站关键字" class="form-control" type="text" name="site_keywords"
+                                   value="{{ $site_keywords or ''}}">
+                        </div>
+                    </div>
+
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">网站描述</label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="text" name="site_description"
+                                   value="{{ $site_description or '' }}">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">每页数量</label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="number" name="page_size"
+                                   value="{{ $page_size or 7 }}">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">简介图片</label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="text" name="profile_image"
+                                   value="{{ $profile_image or ''}}">
+                        </div>
+                    </div>
+                    <div class="form-group">
+                        <label class="col-sm-2 control-label">背景图片</label>
+                        <div class="col-sm-10">
+                            <input class="form-control" type="text" name="background_image"
+                                   value="{{ $background_image or ''}}">
+                        </div>
+                    </div>
+
+                    {{ csrf_field() }}
+                    <div class="form-group">
+                        <div class="col-sm-10 col-sm-offset-2">
+                            <button type="submit" class="btn bg-primary">
+                                保存
+                            </button>
                         </div>
                     </div>
                 </div>
