aab4909a5b3ea60e12451dbe9c0988977bdb5f98
lufficc <luffy.lcc@gmail.com>
2016-10-08 00:42:28 +0800

    if you don't set github username ,then project page will not show

--- resources/views/layouts/header.blade.php
+++ resources/views/layouts/header.blade.php
@@ -26,7 +26,9 @@
             <div class="collapse navbar-collapse fix-top" id="blog-navbar-collapse">
                 <ul class="nav navbar-nav">
                     <li><a class="menu-item" href="{{ route('achieve') }}">归档</a></li>
-                    <li><a class="menu-item" href="{{ route('projects') }}">项目</a></li>
+                    @if(XblogConfig::getValue('github_username'))
+                        <li><a class="menu-item" href="{{ route('projects') }}">项目</a></li>
+                    @endif
                     <li><a class="menu-item" href="{{ route('page.about') }}">关于</a></li>
                 </ul>
                 <ul class="nav navbar-nav navbar-right blog-navbar">
