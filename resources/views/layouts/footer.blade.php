b61ba183eee9acd3adbae467fd396f0d82ea1359
lufficc <luffy.lcc@gmail.com>
2016-10-09 01:06:27 +0800

    fix bug:Undefined variable: site_title

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -4,7 +4,7 @@
         <div class="container">
             <div class="row">
                 <div class="col-sm-12">
-                    <span>Copyright © <a href="{{ route('index') }}">{{ $site_title }}</a></span> |
+                    <span>Copyright © <a href="{{ route('index') }}">{{ $site_title or '' }}</a></span> |
                     <span>蜀ICP备16008148号-2</span> |
                     <span><a href="https://github.com/lufficc/Xblog"><i class="fa fa-github fa-fw"></i></a> </span>
                 </div>
