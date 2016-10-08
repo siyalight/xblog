8ec196d008d2d41702fb28096bf504e5a2c877c4
lufficc <luffy.lcc@gmail.com>
2016-10-08 15:03:04 +0800

    add dynamic footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,14 +1,14 @@
 @include('partials.delete_modal')
 <footer class="footer" id="footer">
-   <div class="copyright">
-      <div class="container">
-         <div class="row">
-            <div class="col-sm-12">
-               <span>Copyright © <a href="https://lufficc.com/">聪聪的网站</a></span> |
-               <span>蜀ICP备16008148号-2</span> |
-               <span><a href="https://github.com/lufficc/laravel-blog"><i class="fa fa-github fa-fw"></i></a> </span>
+    <div class="copyright">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <span>Copyright © <a href="{{ route('index') }}">{{ $site_title }}</a></span> |
+                    <span>蜀ICP备16008148号-2</span> |
+                    <span><a href="https://github.com/lufficc/Xblog"><i class="fa fa-github fa-fw"></i></a> </span>
+                </div>
             </div>
-         </div>
-      </div>
-   </div>
+        </div>
+    </div>
 </footer>
