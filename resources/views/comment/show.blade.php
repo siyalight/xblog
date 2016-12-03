778c5dea7d0f1d1e67e48e0b29e418ce0be28d2e
lufficc <luffy.lcc@gmail.com>
2016-12-03 12:35:56 +0800

    update css

--- resources/views/comment/show.blade.php
+++ resources/views/comment/show.blade.php
@@ -21,16 +21,16 @@
                 </span>
                 <span class="comment-operation pull-right">
                     @can('manager',$comment)
-                        <a href="javascript:void (0)" data-method="delete" data-modal-target="这条评论"
+                        <a title="删除" href="javascript:void (0)" data-method="delete" data-modal-target="这条评论"
                            data-url="{{ route('comment.destroy',$comment->id) }}">
                             <i class="fa fa-trash-o fa-fw"></i>
                         </a>
-                        <a style="text-decoration: none"
+                        <a title="编辑" style="text-decoration: none"
                            href="{{ route('comment.edit',[$comment->id,'redirect'=>(isset($redirect) && $redirect.'#'.$loop->index ? $redirect : '')]) }}">
                             <i class="fa fa-pencil fa-fw"></i>
                         </a>
                     @endcan
-                    <a href="javascript:void (0);" onclick="replySomeone('{{ $comment->username }}')">
+                    <a title="回复" href="javascript:void (0);" onclick="replySomeone('{{ $comment->username }}')">
                         <i class="fa fa-reply fa-fw"></i>
                     </a>
             </span>
