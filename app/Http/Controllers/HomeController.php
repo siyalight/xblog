a96fbfe60c1348bcfbeab5882eb9191935a10349
lufficc <luffy.lcc@gmail.com>
2016-12-03 12:52:38 +0800

    search

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -28,7 +28,9 @@ class HomeController extends Controller
     public function search(Request $request)
     {
         $key = $request->get('q');
-        $posts = Post::where('title', 'like', "%$key%")->get();
+        $title_posts = Post::where('title', 'like', "%$key%")->get();
+        $desc_posts = Post::where('description', 'like', "%$key%")->get();
+        $posts = $title_posts->merge($desc_posts);
         return view('search', compact('posts'));
     }
 
