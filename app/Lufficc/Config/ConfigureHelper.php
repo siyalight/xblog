55a75ae1876b00cefdcf29e348e26a1ab5bb0e52
lufficc <luffy.lcc@gmail.com>
2017-03-04 16:50:27 +0800

    update

---
+++ app/Lufficc/Config/ConfigureHelper.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lufficc
+ * Date: 2017/3/4
+ * Time: 14:51
+ */
+
+namespace Lufficc\Config;
+
+
+class ConfigureHelper
+{
+
+}
---
+++ app/Lufficc/Exception/UnConfigurableException.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: lufficc
+ * Date: 2017/3/4
+ * Time: 15:30
+ */
+
+namespace Lufficc\Exception;
+
+
+class UnConfigurableException
+{
+
+}
