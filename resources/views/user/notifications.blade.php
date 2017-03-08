1a8e679be2517719934f2622b5e7a7ea7386b892
Lufficc Li <lufficc.li@sap.com>
2017-03-08 16:34:34 +0800

    fix error

--- resources/views/user/notifications.blade.php
+++ resources/views/user/notifications.blade.php
@@ -6,13 +6,15 @@
             <div class="col-md-12">
                 <div class="widget widget-default">
                     <div class="widget-header">
-                        <h6><i class="fa fa-comments fa-fw"></i>通知</h6>
-                        @if($notifications->isNotEmpty())
-                            <a class="btn btn-info" role="button"
-                               href="{{ route('user.readNotification',"all") }}">
-                                全部已读
-                            </a>
-                        @endif
+                        <h6><i class="fa fa-comments fa-fw"></i>
+                            通知
+                            @if($notifications->isNotEmpty())
+                                <a class="btn btn-info" role="button" style="display: inline;margin-left: 20px"
+                                   href="{{ route('user.readNotification',"all") }}">
+                                    全部已读
+                                </a>
+                            @endif
+                        </h6>
                     </div>
                     <div class="widget-body">
                         @if($notifications->isEmpty())
@@ -31,38 +33,39 @@
                                 </thead>
                                 <tbody>
                                 @foreach($notifications as $notification)
-                                    <?php $notificationData = null;?>
                                     @if($notification->data)
-                                        <?php $notificationData = $notification->data;?>
+                                        <?php $notificationData = $notification->data?>
+                                        <tr class="">
+                                            <td>
+                                                @if($notificationData['user_id'])
+                                                    <a href="{{ route('user.show',$notificationData['username']) }}">{{ $notificationData['username'] }}</a>
+                                                @else
+                                                    {{ $notificationData['username'] }}
+                                                @endif
+                                            </td>
+                                            <td>
+                                                <a href="mailto:{{ $notificationData['email'] }}">{{ $notificationData['email'] }}</a>
+                                            </td>
+                                            <td>
+                                                @if("App\\Notifications\\ReceivedComment" == $notification->type)
+                                                    评论
+                                                @elseif("App\\Notifications\\MentionedInComment" == $notification->type)
+                                                    提到了你
+                                                @elseif("App\\Notifications\\BaseNotification" == $notification->type)
+                                                    基本提醒
+                                                @endif
+                                            </td>
+                                            <td data-toggle="tooltip" data-placement="top"
+                                                title="{{ $notificationData['content'] }}">{!! $notificationData['content'] !!}</td>
+                                            <td>{{ $notificationData['ip_id']?$notificationData['ip_id']:'NONE' }}</td>
+                                            <td>
+                                                <a class="btn btn-info"
+                                                   href="{{ route('user.readNotification',$notification->id) }}">
+                                                    已读
+                                                </a>
+                                            </td>
+                                        </tr>
                                     @endif
-                                    <tr class="">
-                                        <td>
-                                            @if($notificationData['user_id'])
-                                                <a href="{{ route('user.show',$notificationData['username']) }}">{{ $notificationData['username'] }}</a>
-                                            @else
-                                                {{ $notificationData->username }}
-                                            @endif
-                                        </td>
-                                        <td><a href="mailto:{{ $notificationData['email'] }}">{{ $notificationData['email'] }}</a></td>
-                                        <td>
-                                            @if("App\\Notifications\\ReceivedComment" == $notification->type)
-                                                评论
-                                            @elseif("App\\Notifications\\MentionedInComment" == $notification->type)
-                                                提到了你
-                                            @elseif("App\\Notifications\\BaseNotification" == $notification->type)
-                                                基本提醒
-                                            @endif
-                                        </td>
-                                        <td data-toggle="tooltip" data-placement="top"
-                                            title="{{ $notificationData['content'] }}">{!! $notificationData['content'] !!}</td>
-                                        <td>{{ $notificationData['ip_id']?$notificationData['ip_id']:'NONE' }}</td>
-                                        <td>
-                                            <a class="btn btn-info"
-                                               href="{{ route('user.readNotification',$notification->id) }}">
-                                                已读
-                                            </a>
-                                        </td>
-                                    </tr>
                                 @endforeach
                                 </tbody>
                             </table>
