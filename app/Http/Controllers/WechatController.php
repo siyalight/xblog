ff0a4e7df4b333ed586a1c2776b39728d1bc8434
siya <laisiyuan@meimiao.net>
2017-06-13 19:14:03 +0800

    微信公众平台

--- app/Http/Controllers/WechatController.php
+++ app/Http/Controllers/WechatController.php
@@ -17,13 +17,46 @@ class WechatController extends Controller
     {   
         Log::info('request arrived.'); # 注意：Log 为 Laravel 组件，所以它记的日志去 Laravel 日志看，而不是 EasyWeChat 日志
 
-        $wechat = app('wechat');
-        $wechat->server->setMessageHandler(function($message){
-            return "欢迎关注 overtrue！";
+        $app = app('wechat');
+        $app->server->setMessageHandler(function($message) use ($app){
+            if ($message->MsgType=='event') {
+                $user_openid = $message->FromUserName;
+                
+                if($message->Event=='subscribe'){
+                    $user_info['unionid'] = $message->ToUserName;
+                    $user_info['openid'] = $user_openid;
+                    $userService = $app->user;
+                    $user = $userService->get($user_info['openid']);
+                    $user_info['subscribe_time'] = $user['subscribe_time'];
+                    $user_info['nickname'] = $user['nickname'];
+                    $user_info['avatar'] = $user['headimgurl'];
+                    $user_info['sex'] = $user['sex'];
+                    $user_info['province'] = $user['province'];
+                    $user_info['city'] = $user['city'];
+                    $user_info['country'] = $user['country'];
+                    $user_info['is_subscribe'] = 1;
+
+                    if (WxStudent::weixin_attention($user_info)) {
+                        return '欢迎关注';
+                    }else{
+                        return '您的信息由于某种原因没有保存，请重新关注';
+                    }
+                }else if(WxStudent::weixin_attention($user_info)){
+
+                    //取消关注时执行的操作，（注意下面返回的信息用户不会收到，因为你已经取消关注，但别的操作还是会执行的<如：取消关注的时候，要把记录该用户从记录微信用户信息的表中删掉>）
+                    if (WxStudent::weixin_cancel_attention($user_openid)) {
+                        return '已取消关注';
+                    }
+                }
+
+            }
+            
         });
 
         Log::info('return response.');
 
         return $wechat->server->serve();
     }
+
+
 }
